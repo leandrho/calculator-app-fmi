66db665c9d10f3e64723618fdd3cdba84f8390a7
Leandrho <leandro.coder01@gmail.com>
2024-11-01 11:18:56 -0300

    join into app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 
+import { useCalc } from './hooks/useCalc';
+import { CalcKeyboard } from './components/CalcKeyboard';
+import { CalcDisplay } from './components/CalcDisplay';
+import { CalcHeader } from './components/CalcHeader';
+
 
 function App() {
-
+  const {display, addOperand, setOperation, resolve, del, clear, addDot} = useCalc();
   return (
     <>
+     <main className='flex flex-col gap-6 px-4 py-6 sm:max-w-[540px] '>
+        <CalcHeader/>
+        <CalcDisplay display = { display }/>
+        <CalcKeyboard addOperand={addOperand} setOperation={setOperation} resolve={resolve} del={del} clear={clear} addDot={addDot} />
+     </main>
     </>
   )
 }
