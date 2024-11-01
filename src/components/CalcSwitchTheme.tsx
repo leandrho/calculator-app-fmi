c0f0e7ef8846efd67afd7142bee15497860d6335
Leandrho <leandro.coder01@gmail.com>
2024-11-01 11:17:20 -0300

    add Switch component

---
+++ src/components/CalcSwitchTheme.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+
+export const CalcSwitchTheme = () => {
+    const [curTheme, setCurTheme] = useState(0);
+
+  return (
+    <div className="">
+            <div className="text-[--color-text-screen] text-[11px] font-bold flex justify-between px-3"><span>1</span><span>2</span><span>3</span></div>
+            <div className="relative w-[72px] h-6 flex items-center px-1 bg-[--color-toggle-keypad-bg] rounded-full">
+                <div className={ "absolute w-5 h-5 bg-[--color-key-bg-toggle] rounded-full transition-all filter-bright cursor-pointer " +
+                                (curTheme===0?" translate-x-[0px] ":" ") +
+                                (curTheme===1?" translate-x-[22px] ":" ") +
+                                (curTheme===2?" translate-x-[44px] ":" ") }
+                                >
+                    
+                </div>
+            </div>
+    </div>
+  )
+}
